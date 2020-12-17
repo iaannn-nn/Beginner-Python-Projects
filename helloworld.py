a6ea8f5f7802f1d72e6c6b88729ab7a7bd1ad9f9
yen <hoangyenmailbox@gmail.com>
2020-12-17 09:27:39 +0700

    start learn python

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print("Hello, world")
+
+if 5 > 2:
+	print("1.Five is greater than two")
+
+if 5 > 2:
+	print("2.Five is greater than two")
+if 5 > 2:
+		print("3.Five is greater than two!")
---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation (aka how to put string together)
+# suppose we want to create a string that says "subscribe to _____"
+
+youtuber = "Kylie Ying" # some string variable
+
+# a few ways to do this
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+#madlib = f"Computer programming is so {}"
