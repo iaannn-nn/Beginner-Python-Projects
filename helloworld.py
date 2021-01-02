84e2e74b33879a5851f234a9533f51067ab3e8e5
yen <hoangyenmailbox@gmail.com>
2021-01-02 16:46:07 +0700

    python project 1-madlibs

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,9 @@ if 5 > 2:
 	print("1.Five is greater than two")
 
 if 5 > 2:
-	print("2.Five is greater than two")
+	print("2.Five is greater than two") #this is a comment. 
 if 5 > 2:
 		print("3.Five is greater than two!")
+
+#This is a comment
+print("Cheers, Mate")
